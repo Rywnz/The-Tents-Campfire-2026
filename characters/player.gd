d04a2f3a0d4ae4b7b2b1416ebf8a20dbd5e8ce3a
NO7-ARNAV <arnavdhawadeau@gmail.com>
2026-02-28 21:09:17 +1100

    boss 1 basic mechs

--- characters/player.gd
+++ characters/player.gd
@@ -8,35 +8,45 @@ const JUMP_VELOCITY = -250.0
 
 @onready var sprite = $AnimatedSprite2D
 
+func _ready():
+	add_to_group("player")
+
 func _physics_process(delta: float) -> void:
-	# Add gravity
+	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump
+	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get movement input
+	# Movement
 	var direction: int = Input.get_axis("left", "right")
 
 	if direction:
 		velocity.x = direction * speed
-		
-		# Flip sprite
-		if direction < 0:
-			sprite.flip_h = true
-		else:
-			sprite.flip_h = false
+		sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
 
-	# Animation logic
+	# Animations
 	if not is_on_floor():
 		sprite.play("jump")
 	elif direction:
 		sprite.play("run")
 	else:
 		sprite.play("idle")
+
+
+func take_damage(amount):
+	health -= amount
+	print("Player Health:", health)
+
+	if health <= 0:
+		die()
+
+func die():
+	print("Player Died")
+	queue_free()
